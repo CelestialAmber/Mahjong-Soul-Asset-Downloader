3aacd95810c31f8e6f256ab02e615b1403fbdc30
Amber Brault <celestialamber1@gmail.com>
2022-02-27 17:47:47 -0500

    Add base files

---
+++ MajsoulAssetDownload/LqcFileDecoder.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.IO;
+using Lq.Config;
+using System.Linq;
+using System.Collections.Generic;
+using Google.Protobuf;
+
+namespace MajsoulAssetDownload
+{
+    public class LqcFileDecoder
+    {
+        /*
+        Decodes lqc.lqbin, which is a protobuf file storing most of the game's text and other files
+        in the form of csv files. The csv data inside the file is itself also stored as protobuf data,
+        with the schemas for it also being stored in the file. The proto file that the game uses has been
+        converted to a C# script using Google's compiler tool (see Config.cs). This should maybe be changed
+        so it can use the proto file directly, either by compiling it during runtime to a C# file, or
+        some other way.
+        */
+        public static void DecodeConfigTablesFile()
+        {
+            //Load the protobuf file, which contains most of the game's text
+            FileStream input = File.OpenRead("Assets/v" + Program.version + "/res/config/lqc.lqbin");
+
+            if (!Directory.Exists("csvfiles")) Directory.CreateDirectory("csvfiles");
+
+            //Use the compiled C# script to parse the data from the file
+            ConfigTables configTables = ConfigTables.Parser.ParseFrom(input);
+
+            SheetData[] sheetDataArray = configTables.Datas.ToArray();
+            TableSchema[] tableSchemas = configTables.Schemas.ToArray();
+            List<SheetSchema> sheetSchemasList = new List<SheetSchema>();
+
+            for (int i = 0; i < tableSchemas.Length; i++)
+            {
+                TableSchema tableSchema = tableSchemas[i];
+
+                //Console.WriteLine(tableSchema.Name);
+
+                for (int j = 0; j < tableSchema.Sheets.Count; j++)
+                {
+                    SheetSchema sheetSchema = tableSchema.Sheets[j];
+                    sheetSchemasList.Add(sheetSchema);
+                }
+            }
+
+            for (int i = 0; i < sheetDataArray.Length; i++)
+            {
+                ConvertSheetDataToCSV(sheetDataArray[i], sheetSchemasList[i]);
+            }
+
+        }
+
+        public static void ConvertSheetDataToCSV(SheetData sheetData, SheetSchema sheetSchema)
+        {
+            List<string> fileLines = new List<string>();
+            string table = sheetData.Table;
+            string sheet = sheetData.Sheet;
+            ByteString[] byteStrings = sheetData.Data.ToArray();
+            Field[] fields = sheetSchema.Fields.ToArray();
+            int fieldsAmount = fields.Length;
+            List<byte[]> sheetRowsData = new List<byte[]>();
+
+            foreach (ByteString byteString in byteStrings)
+            {
+                sheetRowsData.Add(byteString.ToByteArray());
+            }
+
+            int numberOfEntries = sheetRowsData.Count;
+
+            string csvFieldsString = "";
+
+            for (int i = 0; i < fieldsAmount; i++)
+            {
+                csvFieldsString += fields[i].FieldName + (i < fieldsAmount - 1 ? ", " : "");
+            }
+
+            //Add the top category csv row
+            fileLines.Add(csvFieldsString);
+
+            //Decrypt each row
+            for (int i = 0; i < numberOfEntries; i++)
+            {
+                byte[] rowData = sheetRowsData[i];
+                int index = 0;
+
+
+                //Console.WriteLine("Bytes: " + Convert.ToHexString(rowData));
+
+                string rowString = "";
+
+                for (int j = 0; j < fieldsAmount; j++)
+                {
+                    Field field = fields[j];
+
+                    string name = field.FieldName;
+                    string type = field.PbType;
+                    uint pbIndex = field.PbIndex;
+                    uint arrayLength = field.ArrayLength;
+                    uint typeVal = 0;
+
+                    //Console.WriteLine("name: {0}, type: {1}, array length: {2}, current offset: {3}", name, type, arrayLength, index);
+
+                    //If the array length is 0, there's only a single element
+                    if (arrayLength == 0){
+                        typeVal = ReadUInt32(rowData, ref index);
+                        string s = ReadAndConvertVariableToString(rowData, ref index, type);
+                        rowString += s;
+                    }else{
+                        //If it's an array, but not a string array, there is an extra byte storing the length of the array
+                        if (type != "string")
+                        {
+                            typeVal = ReadUInt32(rowData, ref index);
+                            index++;
+                        }
+
+                        rowString += "[";
+
+                        for (int k = 0; k < arrayLength; k++){
+                            //For some reason, string arrays are stored as strings one after another with the type bytes, so skip it each time 
+                            if (type == "string")
+                            {
+                                typeVal = ReadUInt32(rowData, ref index);
+                            }
+                            string s = ReadAndConvertVariableToString(rowData, ref index, type);
+                            rowString += s + (k < arrayLength - 1 ? ", " : "");
+                        }
+
+                        rowString += "]";
+                    }
+
+                    if (j < fieldsAmount - 1) rowString += ", ";
+                }
+
+                //Add the line to the list
+                fileLines.Add(rowString);
+            }
+
+
+            File.WriteAllLines("csvfiles/" + sheet + ".csv", fileLines);
+        }
+
+        static string ReadAndConvertVariableToString(byte[] rowData, ref int index, string type)
+        {
+            string result = "";
+
+            switch (type)
+            {
+                case "uint32":
+                case "int32":
+                        uint val = ReadUInt32(rowData, ref index);
+                        result = val.ToString();
+                    break;
+                case "string":
+                        string s = ReadString(rowData, ref index);
+                        result = "\"" + s + "\"";
+                    break;
+                case "float":
+                    float f = ReadFloat(rowData, ref index);
+                    result = f.ToString();
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+
+            return result;
+        }
+
+        //Reads a uint32 value in the protobuf format
+        static uint ReadUInt32(byte[] bytes, ref int index)
+        {
+            int num = 0;
+            bool lastGroup = false;
+            int offset = 0;
+
+            while (lastGroup == false)
+            {
+                lastGroup = true;
+
+                //If the highest bit is 1, there are more bytes to come
+                if((bytes[index] & 0x80) == 0x80)
+                {
+                    lastGroup = false;
+
+                    //Toggle the highest bit
+                    bytes[index] ^= 0x80;
+
+                    num += bytes[index] << offset;
+                    offset += 7;
+                }
+                else
+                {
+                    //Otherwise, this is the last group
+                    num += bytes[index] << offset;
+                }
+
+                index++;
+
+            }
+
+            return (uint)num;
+        }
+
+        static string ReadString(byte[] bytes, ref int index)
+        {
+            int length = (int)ReadUInt32(bytes,ref index);
+            string s = System.Text.Encoding.Default.GetString(bytes.Skip(index).Take(length).ToArray());
+            index += length;
+            return s;
+        }
+
+        static float ReadFloat(byte[] bytes, ref int index)
+        {
+            uint intVal = BitConverter.ToUInt32(bytes.Skip(index).ToArray(),0);
+            float floatVal = BitConverter.UInt32BitsToSingle(intVal);
+            index += 4;
+            return floatVal;
+        }
+
+    }
+}
---
+++ MajsoulAssetDownload/Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace MajsoulAssetDownload
+{
+
+    public class AssetPathDictionary
+    {
+        public Dictionary<string, Dictionary<string, string>> res;
+    }
+
+    public class AssetPathData
+    {
+        public string path;
+        public string versionPrefix;
+
+        public AssetPathData(string path, string versionPrefix)
+        {
+            this.path = path;
+            this.versionPrefix = versionPrefix;
+        }
+    }
+
+    class Program
+    {
+        public static string version = "0.10.83.w";
+        public static AssetPathData[] assetsPathData;
+
+        static void Main(string[] args)
+        {
+            string resJsonFilename = "resversion" + version + ".json";
+
+            //Download the corresponding resversion json file if it hasn't already been downloaded
+            if (!File.Exists(resJsonFilename))
+            {
+                using (WebClient client = new WebClient())
+                {
+                    DownloadAsset("http://mahjongsoul.game.yo-star.com/" + resJsonFilename, resJsonFilename, client);
+                }
+            }
+
+            LoadAssetPathsJSONFile();
+
+            /*
+            using(WebClient client = new WebClient()){
+                for(int i = 0; i < assetsPathData.Length; i++) {
+                    AssetPathData assetPathData = assetsPathData[i];
+                    string path = assetPathData.path;
+                    string versionPrefix = assetPathData.versionPrefix;
+                    //Determine the right prefix to use
+                    string prefix = path.StartsWith("en/") || path.Contains("/en/") || path.Contains("/chs_t/") || path.StartsWith("chs_t") ? "https://mahjongsoul.game.yo-star.com/" : path.Contains("/jp/") || path.StartsWith("jp/") ? "https://game.mahjongsoul.com/" : "https://game.maj-soul.com/1/";
+                    string url = prefix + versionPrefix + "/" + path;
+                    string savePath = "Assets/" + versionPrefix + "/" + path;
+
+                    if (path.Contains(".lnk"))
+                    {
+                        //Console.WriteLine("Unused lnk file, skipping");
+                        continue;
+                    }
+
+                    if (File.Exists(savePath))
+                    {
+                        //Console.WriteLine("File already downloaded, skipping");
+                        continue;
+                    }
+
+                    //Skip non-english files
+                    if (path.StartsWith("myres") || path.StartsWith("extendRes") || path.Contains("jp/") || path.Contains("chs_t/") || (path.StartsWith("res/atlas") && !path.Contains("en/") && !path.Contains("bitmapfont"))) continue;
+
+                    if(path.Contains("png")) Console.WriteLine("Downloading " + path + " (" + (i + 1) + "/" + assetsPathData.Length + ")");
+                    DownloadAsset(url, savePath, client);
+                }
+            }
+            */
+
+
+            LqcFileDecoder.DecodeConfigTablesFile();
+        }
+
+        public static void LoadAssetPathsJSONFile()
+        {
+            Console.WriteLine("Loading resource json file...");
+
+            string data = File.ReadAllText("resversion" + version + ".json");
+            AssetPathDictionary assetPathDictionary = JsonConvert.DeserializeObject<AssetPathDictionary>(data);
+
+            int length = assetPathDictionary.res.Count;
+            string[] assetPaths = assetPathDictionary.res.Keys.ToArray();
+            string[] versionPrefixes = new string[length];
+            Dictionary<string, string>[] prefixDictionaries = assetPathDictionary.res.Values.ToArray();
+
+            for (int i = 0; i < length; i++)
+            {
+                versionPrefixes[i] = prefixDictionaries[i]["prefix"];
+            }
+
+            assetsPathData = new AssetPathData[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                assetsPathData[i] = new AssetPathData(assetPaths[i], versionPrefixes[i]);
+            }
+
+            Console.WriteLine("Done!");
+
+        }
+
+        public static void DownloadAsset(string url, string path, WebClient client)
+        {
+            try
+            {
+                string extension = path.Substring(path.LastIndexOf(".") + 1);
+
+                //Console.WriteLine(url);
+
+                if (path.Contains("/"))
+                {
+                    string pathTemp = path.Substring(0, path.LastIndexOf('/')); //Remove the file name
+
+                    if (pathTemp != "" && !Directory.Exists(pathTemp)) Directory.CreateDirectory(pathTemp);
+                }
+
+                byte[] data = client.DownloadData(url);
+
+                //If the current asset is an image, and is inside an extendRes folder, then it's encrypted
+                if (extension == "png" || extension == "jpg")
+                {
+
+                    /*
+                    If the first byte is C0 (encrypted png file) or B6 (encrypted jpg file), the image is encrypted
+                    if (data[0] == 0xC0 || data[0] == 0xB6)
+                    {
+                    }
+                    */
+
+                    //If the file is in an extendRes folder, it's encrypted
+                    if (path.Contains("extendRes"))
+                    {
+                        //Console.WriteLine("Detected encrypted image, decrypting image");
+                        DecryptAsset(ref data, path);
+                    }
+                }
+
+                File.WriteAllBytes(path, data);
+            }
+            catch (WebException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public static void DecryptAsset(ref byte[] data, string path)
+        {
+            for (int i = 0; i < data.Length; i++)
+            {
+                data[i] ^= 73;
+            }
+        }
+    }
+}
